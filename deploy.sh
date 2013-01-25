6f24a77e70e11a1bfd130faba809f79be301f716
Bui Phi Quan <rongxanh2052004@yahoo.com>
2013-01-26 00:11:09 +0700

    asd

--- deploy.sh
+++ deploy.sh
@@ -29,6 +29,6 @@
 # sudo node app.js
 
 # ssh -i .ssh/bpquanpublic.pem ec2-user@ec2-175-41-185-47.ap-southeast-1.compute.amazonaws.com 'mongod --dbpath mongodb/data'
-pkill node -f
+pkill node
 mongod --dbpath ~/mongodb/data >> production.log 2>&1 &
 sudo node app.js >> production.log 2>&1 &
