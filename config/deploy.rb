1efc50d7c4a8b439252c9e990948f82411fc8e23
Bui Phi Quan <rongxanh2052004@yahoo.com>
2013-01-26 15:55:06 +0700

    asd

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,31 +2,36 @@ set :application, "Smartjsc"
 set :user, "ec2-user"
 set :runner, user
 # set :repository, "svn+ssh://root@118.70.129.253/svn/alley_labs/SolitaireRush/trunk"
-set :deploy_to, "/home/ec2-user/www/#{application}"
+set :deploy_to, "/home/ec2-user/www/smartjsc"
 # set :revision, ENV['revision'] if ENV['revision']
 # set :keep_releases, 5
 set :use_sudo, true
 ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/bpquanpublic.pem"]
 
-set :smartjsc_server, "ec2-175-41-185-47.ap-southeast-1.compute.amazonaws.com"
+set :scm, :git
+set :repository, "git@github.com:markofevil3/smartjsc.git"
+set :branch, "stable"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+# set :ssh_options, { :forward_agent => true }
+
+set :smartjsc_server, "ec2-54-251-45-255.ap-southeast-1.compute.amazonaws.com"
 role :app, smartjsc_server
 
 namespace :smartjsc do
   after "smartjsc:update", "smartjsc:restart"
 
   task :update, :roles => :app do
-    run "cd #{current_path}; git pull"
+    run "cd /home/ec2-user/www/smartjsc; git pull"
   end
 
   task :start, :roles => :app do
-    run "cd #{current_path}; mongod --dbpath ~/mongodb/data >> #{shared_path}/production.log 2>&1 &"
-    # run "cd #{current_path}; sudo NODE_ENV=production node app.js >> #{shared_path}/production.log 2>&1 &"
-    run "cd #{current_path}; ./start.sh"
+    run "cd /home/ec2-user/www/smartjsc; mongod --dbpath ~/mongodb/data >> #{shared_path}/production.log 2>&1 &"
+    run "cd /home/ec2-user/www/smartjsc; ./start.sh >> #{shared_path}/production.log 2>&1 &"
   end
 
   task :stop, :roles => :app do
-    # run "cd #{current_path}; chmod a+x stop.sh; ./stop.sh >> #{shared_path}/log/production.log 2>&1 &"
-    run "cd #{current_path}; sudo pkill node >> #{shared_path}/production.log 2>&1 &"
+    run "cd /home/ec2-user/www/smartjsc; sudo pkill node >> #{shared_path}/production.log 2>&1 &"
   end
 
   task :restart, :roles => :app do
--- start.sh
+++ start.sh
@@ -6,5 +6,4 @@ java -jar node_modules/yuicompressor/yuicompressor-2.4.2.jar public/javascripts 
 cat public/minified/*.js >public/javascripts/minified.js
 rm -f public/minified/*.js
 
-mongod --dbpath ~/mongodb/data >> production.log 2>&1 &
-sudo node app.js >> production.log 2>&1 &
+sudo node app.js
